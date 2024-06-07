d1a7e42b888cea62f7f25f12dc7da7199f30b0af
PratikRaut16 <162879802+PratikRaut16@users.noreply.github.com>
2024-06-07 14:11:40 +0000

    javascript notes here javascript

---
+++ Datatypes/datatypes.js
@@ -0,0 +1,27 @@
+// "use strict"; // treat all js code as newer version 
+
+// alert(3 + 3) // we are using node js not browser 
+
+/* console.log(3
+     + 3 ) */        // code redability should be high
+
+ // console.log("pratik")
+
+ let name = "Pratik"
+ console.log(typeof(name));
+ let number = 12344
+ console.log(typeof(number));
+ let isLoggedIn = false
+ console.log(typeof(isLoggedIn));
+ 
+ // number => 2 to power 53
+ // bigint 
+ // string => " "
+ // boolean => true/false
+ // null =>standalone value
+ // undefined => 
+ // symbol => unique
+ 
+ // object 
+ 
+ // console.log(typeof null); // object 
